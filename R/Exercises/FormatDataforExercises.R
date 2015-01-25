8b5ee2c94b067441f0919d798ea36181a041b68a
Lauren Hallett <lauren.hallett@berkeley.edu>
2015-01-24 21:23:07 -0800

    formats the messy little spokane file

--- R/Exercises/FormatDataforExercises.R
+++ R/Exercises/FormatDataforExercises.R
@@ -1,3 +1,7 @@
+library(tidyr)
+library(dplyr)
+library(lubridate)
+
 ## There were a few date/time/site combinations with repeat values for the calispell creek data
 ## I removed them so we didn't have problems with the spread functin
 
@@ -8,15 +12,30 @@ wtemp2<-gather(wtemp, site, temperature, calispell_temp:winchester_temp) %>%
 
 # write.csv(wtemp2, "Calispell Creek and Tributary Temperatures2.csv", row.names=F)
 
+## GENERATE MESSY FISH DATA
 dat<-tbl_df(read.csv("LowSpokaneClean.csv")) %>%
   mutate(FishLength_Weight=paste(FinLength, Weight, sep="_")) %>%
   select(Date, Year, Pass, Site.No., FishNo, Species, FishLength_Weight, ScaleAge, CapturedFloyTagNo, AppliedFloyTagNo,
-         CapturedPITTagNo, ActivePITTagNo, AgeMethod)
-dat
+       AgeMethod)
 
+efdat<-tbl_df(read.csv("Lower Spokane River_2013_effort.csv"))
+  names(efdat)=c("Pass", "Site", "Start_lat", "Start_long", "End_lat",
+                 "End_long", "Date", "Time", "Voltage", "Percent", "PulseFreq", "Amps", "Effort",
+                 "WaterTemp", "Observers", "Boat", "GeneralLocation", "Notes")
+efdat <- mutate(efdat, Date=mdy(Date)) %>%
+  arrange( Site)
 
+names(dat)=c("Date", "Year", "Pass", "Site", "FishNo", "Species", "FishLength_Weight", "ScaleAge", "CapturedFloyTagNo",
+             "AppliedFloyTagNo", "AgeMethod")
+dat<-mutate(dat, Date=mdy(Date))
 
-# %>%
-# separate(site, c("site", "measurement")) %>%
-# select(-measurement)
+tog<-tbl_df(merge(dat, efdat))
+
+tog<-tog %>%
+  select(Date, Site, Pass, FishNo, Species, FishLength_Weight, ScaleAge, CapturedFloyTagNo, AppliedFloyTagNo, 
+          Effort) %>%
+  arrange(Site)
+
+write.csv(tog, "LittleSpokane_Messy.csv", row.names=F)
+
 

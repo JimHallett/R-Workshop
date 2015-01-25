40614a23a797d70c54a69888d7c6b82ef9d132f6
JimHallett <jhallett@ewu.edu>
2015-01-24 21:34:23 -0800

    added questions and dividers

--- R/Exercises/SimpleStats.R
+++ R/Exercises/SimpleStats.R
@@ -7,13 +7,21 @@
 
 ## We'll use some fish data from Chuck Lee at WDFW for the Lower Spokane River.
 
-## First we'll read in the data.
+################################################################################
+### Preliminary: Reading the data                                            ###
+################################################################################
+
+## First we'll read in the data and get it ready to use.
 
 SpokaneFish <- read.csv(file="LowSpokaneClean.csv", header=TRUE, sep=',')
 # Fix dates
 SpokaneFish$Date <- as.Date(SpokaneFish$Date, "%m/%d/%Y")
 # Make it easier to see in the console
 tbl_df(SpokaneFish)
+
+################################################################################
+### EXERCISE 1: Looking at frequencies                                       ###
+################################################################################
 
 # We often want to look at frequency data, for example, the numbers of each fish
 # species in our samples.
@@ -22,25 +30,31 @@ tbl_df(SpokaneFish)
 
 table(SpokaneFish$Species)  # Prints number of each species to the console
 
-# We can also store this table
+# Create a table of captures at each sampling site.
+
+# We can also store tables, as done here for fish species:
 
 Species.freq <- table(SpokaneFish$Species)
 
-# We then also allows us to calculate proportions
+# R can then calculate proportions:
 
 prop.table(Species.freq)
 
-# Or round to 2 decimal places
+# We can round to 2 decimal places:
 
-round(prop.table(Species.freq),3)
+round(prop.table(Species.freq), 2)
 
-# Or express as percent by multiplying by 100
+# Is this satisfactory? How might you modify the result?
 
-round(prop.table(Species.freq), 3) * 100
+# Sometimes we'll want to convert to percentages. Can you do that for this example?
+
+################################################################################
+### EXERCISE 2: Looking at quantitative variables                            ###
+################################################################################
 
 # We'll just consider redband trout (RB) to look at quantitative variables
 
-Redband <- SpokaneFish[ which(SpokaneFish$Species=='RB'), ]   #There are several ways to subset data
+Redband <- SpokaneFish[ which(SpokaneFish$Species=='RB'), ]   #This is one of several ways to subset data
 
 # Descriptive statistics are useful for understanding data. The redband data include weights and fin lengths.
 # We'll look at these 2 variables. The built in function summary is useful.
@@ -53,11 +67,16 @@ summary(Redband$Weight)
 summary(Redband)
 
 # There are other descriptors in packages such as psych. This package will have to be installed.
-
+# Type install.packages("psych") in the console.
 library(psych)
 
 describe(Redband$FinLength) 
 describe(Redband$Weight)
+
+
+################################################################################
+### EXERCISE 3: Simple plotting                                              ###
+################################################################################
 
 # We also gain a lot of understanding by graphing our data. We'll start with some quick and simple
 # plots. 
@@ -75,19 +94,24 @@ barplot(speciesplot[order(speciesplot)],
 horiz = TRUE,  
 xlab = "Proportion of total capture")  
 
-# This is obviously very rudimentary and there are other graphics packages that provide greater
-# flexibility. We'll use ggplot2 for this session. Some others including ggvis are still being
-# developed and will provide interactivity. Shiny also provides direct interaction, but requires
-# initial setup - more on that later.
+
+################################################################################
+### EXERCISE 4: Slightly more advanced plotting                              ###
+################################################################################
+
+# The previous plots are obviously very rudimentary and there are other graphics packages that 
+# provide greater flexibility. We'll use ggplot2 for this session. Some others including ggvis
+# are still being developed and will provide interactivity. Shiny also provides direct 
+# interactivity, but requires initial setup - more on that later.
 
 library(ggplot2)
 
 # We'll first look at the relationship between fin length and weight in Spokane River redband.
 # First, a simple scatterplot:
 
-qplot(FinLength, Weight, data = Redband)
+qplot(FinLength, Weight, data = Redband) 
 
-# Note that we can also identify the dataset by using Redband$FinLength. 
+# Note that we can also identify the dataset by using Redband$FinLength, etc. 
 
 # This scatterplot shows a curvilinear relationship between weight and fin length. 
 # We can deal with this in several ways, but here we apply a log transformation to the data.
@@ -126,8 +150,7 @@ weightmodel <- lm(Weight~FinLength + factor(ScaleAge), data=Redband)
 
 qplot(log(FinLength), log(Weight), data = Redband, colour=factor(ScaleAge))
 
-# This is a quick and dirty look seems to support at the relationship between scale age and growth.
-
+# This quick and dirty look seems to support at the relationship between scale age and growth.
 
 # Let's next look at the relationship between fin length (quantitative) and scale age (qualitative).
 # We'll start with a simple histogram. 
@@ -138,28 +161,39 @@ hist(Redband$FinLength)
 
 hist(Redband$FinLength, breaks=100, main="", col="Red")
 
-# This plot suggests that there are at least 3 cohorts based on fin length. 
+# Plot the histgram for redband weight. 
+
+# The histogram of fin lengths suggests that there are at least 3 cohorts.  
 # Let's look at this as a function of scale age.
-# First we create a factor for scale age.
+# First we create a factor for scale age:
 
 scale.f <- factor(Redband$ScaleAge)      #Creates a factor of the scale ages
 
-# Now we'll create a density plot based on this factor. The line width was increased (lwd=3)
+# What does this factor look like?
+
+# Now we'll create a density plot based on this factor. The line width is increased (lwd=3)
 # to make it easier to see.
 
 sm.density.compare(Redband$FinLength, Redband$ScaleAge, lwd=3)
 
 # We can also show this as a whisker plot:
 
-plot(Redband$FinLength ~ scale.f)  
+plot(Redband$FinLength ~ scale.f) 
 
-# Both graphs show some clear separation between some scale age classes and a degree of overlap
-# for the later classes. We can examine this further with analysis of variance (ANOVA). The 
-# question is whether the means of the groups differ.
 
-aovout = aov(Redband$Weight ~ factor(Redband$ScaleAge))  
-summary(aovout)
+################################################################################
+### EXERCISE 5: Testing for differences                                      ###
+################################################################################
 
+
+# Both density and whisker plots for fin length show some clear separation between some 
+# scale age classes and a degree of overlap for the older classes. We can examine this further with 
+# analysis of variance (ANOVA). The question is whether the means of the groups differ.
+
+
+aovout.fl = aov(Redband$FinLength ~ factor(Redband$ScaleAge))
+summary(aovout.fl)
+             
 # This shows us that there are significant differences among the 8 scale age groups.
 # Next we'll do a multiple comparisons test to see where the differences reside.
 
@@ -168,13 +202,12 @@ TukeyHSD(aovout, conf.level = 0.95)
 # This test indicates that scale age groups 0, 1, 2, 3 and 4 are distinct. Groups 4 and 5 overlap
 # and there is substantial overlap amongst the later age groups. 
 
+# Does weight show the same pattern as fin length?
 
 
 
 
 
-
- 
 
 
 
